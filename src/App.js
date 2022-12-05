c124e2e5886f5651af32ed902246cd88f3c7ee2e
yetemegn-telaye <lemma19yet@gmail.com>
2022-12-05 18:49:38 +0300

    Create basic file structure

--- src/App.js
+++ src/App.js
@@ -1,28 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './components/HomePage';
+import CoinDetail from './components/CoinDetail';
+import Coin from './components/Coin';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/coin" element={<Coin />} />
+        <Route path="/coin-details" element={<CoinDetail />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/components/Coin.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Coin = () => (
+  <div>
+    Coin component
+    <button type="button">Click to see coin details</button>
+  </div>
+);
+export default Coin;
