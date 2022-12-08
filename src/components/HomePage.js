52c9d82ade5d3444042a4f08191865e74026578e
yetemegn-telaye <lemma19yet@gmail.com>
2022-12-08 13:26:09 +0300

    Implement live search with coin name

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,9 +1,11 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchCoinsData } from '../redux/coins';
 import Coin from './Coin';
+import '../styles/home.css';
 
 const HomePage = () => {
+  const [search, setSearch] = useState('');
   const dispatch = useDispatch();
   const coinsArr = useSelector((state) => state.coins);
   const shouldFetch = useRef(false);
@@ -15,34 +17,20 @@ const HomePage = () => {
     }
     shouldFetch.current = true;
   }, []);
-  console.log(coinsArr);
   if (coinsArr.length > 0) {
     return (
-      <div>
-        HomePage
-        {coinsArr.map((coin) => (
-          <div key={coin.id}>
-            <h3>
-              {coin.coinName}
-              {' '}
-              (
-              {coin.symbol}
-              )
-              {' '}
-            </h3>
-            <p>
-              Rank:
-              {coin.rank}
-            </p>
-            <p>
-              Price($):
-              {coin.priceUsd}
-            </p>
-          </div>
-        ))}
-        <Coin />
-        <Coin />
-
+      <div className="home-container">
+        <div className="nav">
+          <h4>Home</h4>
+          <form>
+            <input onChange={(e) => setSearch(e.target.value)} placeholder="Search coins" />
+          </form>
+        </div>
+        <div className="coins-container">
+          {coinsArr.filter((coin) => (search.toLowerCase() === '' ? coin : coin.coinName.toLowerCase().includes(search))).map((coin) => (
+            <Coin key={coin.id} coinId={coin.id} coinName={coin.coinName} symbol={coin.symbol} rank={coin.rank} coinPrice={coin.priceUsd} />
+          ))}
+        </div>
       </div>
     );
   }
