b79a48c7582494ff2f90dd88052a47d0df888691
yetemegn-telaye <lemma19yet@gmail.com>
2022-12-09 20:01:58 +0300

    Fix bug

--- src/index.js
+++ src/index.js
@@ -2,11 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-  <h1>Hello</h1>
+    <h1>Hello</h1>
   </React.StrictMode>,
 );
 
